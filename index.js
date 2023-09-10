35855286b58c2589da35acd45e5ead5cfd873af0
hellboyman <123723106+hellboyman@users.noreply.github.com>
2023-09-10 22:33:58 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
-var a = "我有一個兩年的計畫,這網頁也會隨著我不斷學習,會持續更新網頁 \n 2023. /9 /10"
-var b = 'Welcome to My Web!'
-alert(b)
-alert(a)
+function showPassage(mypassage){
+    var  mypassage = "Welcome to My Web";
+    alert(mypassage);
+    function showPassage1 (mypassage1){
+        var mypassage1 = "我有一個兩年的計畫,這網頁也會隨著我不斷學習,會持續更新網頁 \n 2023. /9 /10"
+        alert(mypassage1)
+    }
+    showPassage1()
+}
+showPassage()
